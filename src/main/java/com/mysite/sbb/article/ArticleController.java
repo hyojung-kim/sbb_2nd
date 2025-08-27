7eec65381fdb8a281f65e736cbab4ddec4d16023
hyo-jung_SBS <rlagywnd05@gmail.com>
2025-08-27 16:37:47 +0900

    url 프리픽스

--- src/main/java/com/mysite/sbb/article/ArticleController.java
+++ src/main/java/com/mysite/sbb/article/ArticleController.java
@@ -5,21 +5,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
+@RequestMapping("/article")
 @RequiredArgsConstructor
 @Controller
 public class ArticleController {
     private final ArticleService articleService;
 
-    @GetMapping("/article/list")
+    @GetMapping("/list")
     public String list(Model model) {
         List<Article> artileList = this.articleService.findAll();
         model.addAttribute("articleList", artileList);
         return "/article/article_list";
     }
-    @GetMapping(value = "/article/detail/{id}")
+    @GetMapping(value = "/detail/{id}")
     public String detail(Model model, @PathVariable("id") Integer id) {
         Article article = this.articleService.getArticle(id);
         model.addAttribute("article", article);
