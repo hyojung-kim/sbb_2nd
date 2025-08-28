512f1a9049de7bc826d59e3be865b98755afd1c1
hyo-jung_SBS <rlagywnd05@gmail.com>
2025-08-28 17:39:15 +0900

    답글수정

--- src/main/java/com/mysite/sbb/comment/CommentController.java
+++ src/main/java/com/mysite/sbb/comment/CommentController.java
@@ -6,13 +6,16 @@ import com.mysite.sbb.user.SiteUser;
 import com.mysite.sbb.user.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.security.Principal;
 
@@ -36,6 +39,32 @@ public class CommentController {
         }
         this.commentService.create(article, commentForm.getContent(), siteUser);
         return String.format("redirect:/article/detail/%s", id);
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/modify/{id}")
+    public String answerModify(CommentForm commentForm, @PathVariable("id") Integer id, Principal principal) {
+        Comment comment = this.commentService.getComment(id);
+        if (!comment.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+        commentForm.setContent(comment.getContent());
+        return "article/comment_form";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/modify/{id}")
+    public String answerModify(@Valid CommentForm commentForm, BindingResult bindingResult,
+                               @PathVariable("id") Integer id, Principal principal) {
+        if (bindingResult.hasErrors()) {
+            return "article/answer_form";
+        }
+        Comment comment = this.commentService.getComment(id);
+        if (!comment.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+        this.commentService.modify(comment, commentForm.getContent());
+        return String.format("redirect:/article/detail/%s", comment.getArticle().getId());
     }
 
 }
--- src/main/java/com/mysite/sbb/comment/CommentService.java
+++ src/main/java/com/mysite/sbb/comment/CommentService.java
@@ -1,11 +1,13 @@
 package com.mysite.sbb.comment;
 
+import com.mysite.sbb.DataNotFoundException;
 import com.mysite.sbb.article.Article;
 import com.mysite.sbb.user.SiteUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -18,6 +20,21 @@ public class CommentService {
         comment.setCreateDate(LocalDateTime.now());
         comment.setArticle(article);
         comment.setAuthor(author);
+        this.commentRepository.save(comment);
+    }
+
+    public Comment getComment(Integer id) {
+        Optional<Comment> comment = this.commentRepository.findById(id);
+        if (comment.isPresent()) {
+            return comment.get();
+        } else {
+            throw new DataNotFoundException("answer not found");
+        }
+    }
+
+    public void modify(Comment comment, String content) {
+        comment.setContent(content);
+        comment.setModifyDate(LocalDateTime.now());
         this.commentRepository.save(comment);
     }
 
